5af1bc23efad1b8b0364b92c06b39acabf53fd1c
Noah-611 <nys0611@dankook.ac.kr>
2026-03-01 14:34:59 +0900

    큐

--- silver/큐.py
+++ silver/큐.py
@@ -1,32 +1,32 @@
 import sys
 input = sys.stdin.readline
 
+A=[]
 N = int(input())
-A = []
 
 for i in range(N):
     command = input().split()
-
     if command[0] == 'push':
         A.append(command[1])
-
     elif command[0] == 'pop':
         if not A:
             print(-1)
         else:
-            print(A.pop())
-
-    elif command[0] == 'size':
+            print(A.pop(0))
+    elif command[0]=='size':
         print(len(A))
-
     elif command[0] == 'empty':
         if not A:
             print(1)
         else:
             print(0)
-
-    elif command[0] == 'top':
+    elif command[0] == 'front':
         if not A:
             print(-1)
         else:
-            print(A[-1])
+            print(A[0])
+    elif command[0] == 'back':
+        if not A:
+            print(-1)
+        else:
+            print(A[len(A)-1])
